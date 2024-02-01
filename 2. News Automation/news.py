25c81edbcf0f20ee587a00ce897a625997edb42c
Aditya Mishra <mishraaaditya29@gmail.com>
2024-02-01 15:07:49 +0530

    git commit news.py updated

--- 2. News Automation/news.py
+++ 2. News Automation/news.py
@@ -1,2 +1,6 @@
+# making web driver
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+
+website = "https://www.sofascore.com/"
+path = "/Users/adityamishra/downloads/chromedriver"
